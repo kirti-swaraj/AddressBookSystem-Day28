bb3b008f5ad32832621f3b5c2952a1405d64aee9
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-09 07:13:53 +0530

    [kirti] Add. create new Address Book Data table

---
+++ AddressBook_LINQ/AddressBookDataTable.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookDataTable.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kirti Swaraj"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace AddressBook_LINQ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
+    public class AddressBookDataTable
+    {
+        /// <summary>
+        /// UC 1 : Created new address book table
+        /// </summary>
+        public static DataTable addressBookTable = new DataTable();
+    }
+}
---
+++ AddressBook_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kirti Swaraj"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace AddressBook_LINQ
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book manager!");
+        }
+    }
+}
