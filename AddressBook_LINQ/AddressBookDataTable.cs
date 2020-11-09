d981d8e245218f1866817b5856098bff07f310b9
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-09 14:41:49 +0530

    [kirti] Add . retrieve contacts based on city or state

--- AddressBook_LINQ/AddressBookDataTable.cs
+++ AddressBook_LINQ/AddressBookDataTable.cs
@@ -78,11 +78,27 @@ namespace AddressBook_LINQ
         public static void DeleteContact(string firstName, string lastName)
         {
             //Retrieve the datarow containing given name
-            var x = (from p in addressBookTable.AsEnumerable()
-                     where p.Field<string>("FirstName") == firstName && p.Field<string>("LastName") == lastName
+            var retrievedData = (from p in addressBookTable.AsEnumerable()
+                                 where p.Field<string>("FirstName") == firstName && p.Field<string>("LastName") == lastName
                      select p).FirstOrDefault();
             //Delete the row
-            x.Delete();
+            retrievedData.Delete();
+        }
+
+        /// <summary>
+        /// UC 6 : Retrieves the person from a city or state.
+        /// </summary>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        public static void RetrievePersonFromACityOrState(string city, string state)
+        {
+            var retrievedData = (from p in addressBookTable.AsEnumerable()
+                                 where p.Field<string>("City") == city || p.Field<string>("State") == state
+                                 select p);
+            foreach (var v in retrievedData)
+            {
+                Console.WriteLine($"FirstName:{v.Field<string>("FirstName")}\nLastName:{v.Field<string>("LastName")}\nAddress:{v.Field<string>("Address")}\nCity:{v.Field<string>("City")}\nState:{v.Field<string>("State")}\nZip:{v.Field<int>("Zip")}\nPhoneNumber:{v.Field<double>("PhoneNumber")}\nEmail:{v.Field<string>("Email")}\n");
+            }
         }
     }
 }
